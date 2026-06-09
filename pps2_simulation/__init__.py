38604c5fd7325dc1956ff12ce0038088c6f9d72c
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2026-06-09 18:57:27 +0100

    updated init file

--- pps2_simulation/__init__.py
+++ pps2_simulation/__init__.py
@@ -1,3 +1,6 @@
-# Import from the common.py file
+# Common geometric tools
+from .common import draw_slab, draw_active_passive_slab
+
+# Specific assemblies and semantic taggers
 from .asic_simple import create_silicon_slab
 from .asic_detailed import create_detailed_silicon_slab
